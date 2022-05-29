e976ce4a32b0ea382096e11088ccbca49a70081e
derekhongg <hongderek@gmail.com>
2022-05-29 12:36:14 -0700

    light dark switch added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './components/Home';
 import {createContext, useState} from "react";
+import ReactSwitch from 'react-switch';
 import './App.css';
 
 export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme((current) => (current === "dark" ? "light": "dark"))
@@ -14,6 +15,10 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
+        <div className='switch'>
+          <label className='switch-label'>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+        </div>
         <Home/>
       </div>
     </ThemeContext.Provider>
