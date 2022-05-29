5e27db4f238e29dfc2875dc9a18cf1782db1e596
derekhongg <hongderek@gmail.com>
2022-05-28 22:13:42 -0700

    data added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
